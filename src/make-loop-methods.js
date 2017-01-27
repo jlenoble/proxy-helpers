625b93821825a2846f9b59990566ce916e24a7ca
Jason Lenoble <jason.lenoble@gmail.com>
2017-01-27 18:12:16 +0100

    makeLoopMethod helper
    Allows to generate loop methods individually

--- src/make-loop-methods.js
+++ src/make-loop-methods.js
@@ -1,5 +1,59 @@
 import isEnumerable from './is-enumerable';
 
+export function makeLoopMethod (obj, keys, name) {
+  switch (name) {
+  case 'keys':
+    return function () {
+      return keys;
+    };
+
+  case 'forEach':
+    return function (func) {
+      keys.forEach(key => {
+        func(obj[key], key, obj);
+      });
+    };
+
+  case 'some':
+    return function (func) {
+      return keys.some(key => {
+        return func(obj[key], key, obj);
+      });
+    };
+
+  case 'every':
+    return function (func) {
+      return keys.every(key => {
+        return func(obj[key], key, obj);
+      });
+    };
+
+  case 'map':
+    return function (func) {
+      const res = {};
+      keys.forEach(key => {
+        res[key] = func(obj[key], key, obj);
+      });
+      return res;
+    };
+
+  case 'snapshot':
+    return function () {
+      const res = {};
+      keys.forEach(key => {
+        const value = obj[key];
+        if (isEnumerable(value)) {
+          const meths = makeLoopMethods(value);
+          res[key] = meths.snapshot();
+        } else {
+          res[key] = value;
+        }
+      });
+      return res;
+    };
+  }
+}
+
 export default function makeLoopMethods (obj, keys, _names) {
   const loopMethods = {};
   const names = Object.assign({
@@ -12,63 +66,7 @@ export default function makeLoopMethods (obj, keys, _names) {
   }, _names);
 
   Object.keys(names).forEach(name => {
-    switch (name) {
-    case 'keys':
-      loopMethods[names[name]] = function () {
-        return keys;
-      };
-      break;
-
-    case 'forEach':
-      loopMethods[names[name]] = function (func) {
-        keys.forEach(key => {
-          func(obj[key], key, obj);
-        });
-      };
-      break;
-
-    case 'some':
-      loopMethods[names[name]] = function (func) {
-        return keys.some(key => {
-          return func(obj[key], key, obj);
-        });
-      };
-      break;
-
-    case 'every':
-      loopMethods[names[name]] = function (func) {
-        return keys.every(key => {
-          return func(obj[key], key, obj);
-        });
-      };
-      break;
-
-    case 'map':
-      loopMethods[names[name]] = function (func) {
-        const res = {};
-        keys.forEach(key => {
-          res[key] = func(obj[key], key, obj);
-        });
-        return res;
-      };
-      break;
-
-    case 'snapshot':
-      loopMethods[names[name]] = function () {
-        const res = {};
-        keys.forEach(key => {
-          const value = obj[key];
-          if (isEnumerable(value)) {
-            const meths = makeLoopMethods(value);
-            res[key] = meths.snapshot();
-          } else {
-            res[key] = value;
-          }
-        });
-        return res;
-      };
-      break;
-    }
+    loopMethods[names[name]] = makeLoopMethod(obj, keys, name);
   });
 
   return loopMethods;
