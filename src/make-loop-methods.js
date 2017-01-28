013c16d28da1757f9e0ee14f49f8f006a38575bb
Jason Lenoble <jason.lenoble@gmail.com>
2017-01-28 16:47:36 +0100

    Moved getKeys() to its own file

--- src/make-loop-methods.js
+++ src/make-loop-methods.js
@@ -1,49 +1,5 @@
 import isEnumerable from './is-enumerable';
-
-export function getKeys (obj, keys) {
-  switch (keys) {
-  case 'attributes':
-    return Object.keys(obj).filter(key => {
-      return typeof obj[key] !== 'function';
-    });
-
-  case 'states':
-    return Object.getOwnPropertyNames(obj).filter(key => {
-      return !Object.getOwnPropertyDescriptor(obj, key).enumerable &&
-        typeof obj[key] !== 'function';
-    });
-
-  case 'methods':
-    const instanceMethods = new Set();
-
-    Object.getOwnPropertyNames(obj).filter(key => {
-      return typeof obj[key] === 'function';
-    }).forEach(key => {
-      instanceMethods.add(key);
-    });
-
-    let proto = obj;
-
-    while (1) {
-      proto = Object.getPrototypeOf(proto);
-
-      if (!proto || proto === Object.prototype) {
-        break;
-      }
-
-      Object.getOwnPropertyNames(proto).filter(key => {
-        return key !== 'constructor' && typeof obj[key] === 'function';
-      }).forEach(key => {
-        instanceMethods.add(key);
-      });
-    }
-
-    return [...instanceMethods];
-
-  default:
-    return Object[keys](obj);
-  }
-}
+import getKeys from './get-keys';
 
 export function makeLoopMethod (obj, _keys, name) {
   const keys = getKeys(obj, _keys);
