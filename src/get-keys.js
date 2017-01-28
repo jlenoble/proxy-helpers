1aff486fd6599759db4cee97ed39eed6132c7fd1
Jason Lenoble <jason.lenoble@gmail.com>
2017-01-28 16:52:47 +0100

    getKeys() logic extracted to specific getters

--- src/get-keys.js
+++ src/get-keys.js
@@ -1,42 +1,54 @@
-export default function getKeys (obj, keys) {
-  switch (keys) {
-  case 'attributes':
-    return Object.keys(obj).filter(key => {
-      return typeof obj[key] !== 'function';
-    });
+export function getAttributes (obj) {
+  return Object.keys(obj).filter(key => {
+    return typeof obj[key] !== 'function';
+  });
+}
 
-  case 'states':
-    return Object.getOwnPropertyNames(obj).filter(key => {
-      return !Object.getOwnPropertyDescriptor(obj, key).enumerable &&
-        typeof obj[key] !== 'function';
-    });
+export function getStates (obj) {
+  return Object.getOwnPropertyNames(obj).filter(key => {
+    return !Object.getOwnPropertyDescriptor(obj, key).enumerable &&
+      typeof obj[key] !== 'function';
+  });
+}
 
-  case 'methods':
-    const instanceMethods = new Set();
+export function getMethods (obj) {
+  const instanceMethods = new Set();
 
-    Object.getOwnPropertyNames(obj).filter(key => {
-      return typeof obj[key] === 'function';
+  Object.getOwnPropertyNames(obj).filter(key => {
+    return typeof obj[key] === 'function';
+  }).forEach(key => {
+    instanceMethods.add(key);
+  });
+
+  let proto = obj;
+
+  while (1) {
+    proto = Object.getPrototypeOf(proto);
+
+    if (!proto || proto === Object.prototype) {
+      break;
+    }
+
+    Object.getOwnPropertyNames(proto).filter(key => {
+      return key !== 'constructor' && typeof obj[key] === 'function';
     }).forEach(key => {
       instanceMethods.add(key);
     });
+  }
 
-    let proto = obj;
+  return [...instanceMethods];
+}
 
-    while (1) {
-      proto = Object.getPrototypeOf(proto);
+export default function getKeys (obj, keys) {
+  switch (keys) {
+  case 'attributes':
+    return getAttributes(obj);
 
-      if (!proto || proto === Object.prototype) {
-        break;
-      }
+  case 'states':
+    return getStates(obj);
 
-      Object.getOwnPropertyNames(proto).filter(key => {
-        return key !== 'constructor' && typeof obj[key] === 'function';
-      }).forEach(key => {
-        instanceMethods.add(key);
-      });
-    }
-
-    return [...instanceMethods];
+  case 'methods':
+    return getMethods(obj);
 
   default:
     return Object[keys](obj);
