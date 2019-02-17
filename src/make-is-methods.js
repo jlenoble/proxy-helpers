d46ce46165b88c0f3f4ce205f32075827fb145de
Jason Lenoble <jason.lenoble@gmail.com>
2019-02-17 10:02:52 +0100

    Replaced let by const

--- src/make-is-methods.js
+++ src/make-is-methods.js
@@ -2,7 +2,7 @@ export default function makeIsMethods (obj) {
   const isMethods = {};
 
   ['writable', 'enumerable', 'configurable'].forEach(xable => {
-    let method = 'propertyIs' + xable[0].toUpperCase() + xable.substr(1);
+    const method = 'propertyIs' + xable[0].toUpperCase() + xable.substr(1);
 
     isMethods[method] = function (name) {
       const descriptor = Object.getOwnPropertyDescriptor(obj, name);
